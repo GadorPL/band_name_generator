e90487f30275238d43bed23f2dde24389eed6824
Michał Hewelt <mhewelt9@gmail.com>
2023-11-02 16:51:50 +0100

    complete

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-test commit
+print("Welcome to the Band Name Generator.")
+
+city = input("What's the name of the city you grew up in? ")
+pet_name = input("What's your pet's name? ")
+
+print(f"Your band name could be '{city} {pet_name}'!")
